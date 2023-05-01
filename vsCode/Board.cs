b727cd58407f1566372bcab64241f01a0ce43957
MinkyeongYu <90701558+MinkyeongYu@users.noreply.github.com>
2023-05-02 03:15:46 +0900

    Setting

---
+++ vsCode/Board.cs
@@ -0,0 +1,12 @@
+class Board {
+
+    public int[] _data = new int[25];
+    public List <int> _data2 = new List<int>();
+    public LinkedList <int> _data3 = new LinkedList<int>();
+
+
+    public void init() {
+        
+    }
+}
+
