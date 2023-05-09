46306eb6e02072aa3b19280475089307ff902999
MinkyeongYu <90701558+MinkyeongYu@users.noreply.github.com>
2023-05-10 00:49:19 +0900

    0510

--- vsCode/Board.cs
+++ vsCode/Board.cs
@@ -1,12 +1,69 @@
+class MyList<T> {
+    const int DEFAULT_SIZE = 1;
+    T[] _data = new T[DEFAULT_SIZE];
+
+    // 실제로 사용중인 데이터 개수
+    public int Count = 0;
+    // 예약된 데이터 개수 = 배열의 크기
+    public int Capacity { get { return _data.Length; } }
+    
+    //O(N)이지만, Count가 Capacity보다 클 경우가 드물기 때문에, O(1)로 취급한다. (특이한경우)
+    public void Add(T item) {
+        // 공간이 남아있지 않으면
+        if(Count >= Capacity) {
+            // 공간 확보하기 -> 새 배열 만들기
+            T[] newArr = new T[Count * 2];
+            for(int i=0; i<Count; i++) {
+                // 새 배열에 데이터 옮기기
+                newArr[i] = _data[i];
+            }
+            // _data배열을 newArr 배열로 만들기
+            _data = newArr;
+        }
+        // 공간에 데이터 삽입
+        _data[Count] = item;
+        Count++;
+    }
+    // 인덱서 문법
+    public T this[int index] {
+        get {
+            return _data[index];
+        }
+        set {
+            _data[index] = value;
+        }
+    }
+    // 삭제 함수
+    public void RemoveAt(int index) {
+        for(int i=index; i<Count; i++){
+            //삭제 element 뒤의 element를 한칸씩 앞으로 당긴다.
+            _data[i] = _data[i+1];
+        }
+        //맨 뒤 요소는 쓰레기이기 때문에 0 또는 NULL 값으로 대체함. 
+        _data[Count-1] = default(T);\
+        Count--;
+    }
+}
+
 class Board {
 
+    // 베열
     public int[] _data = new int[25];
-    public List <int> _data2 = new List<int>();
+    // 동적 배열
+    public MyList <int> _data2 = new MyList<int>();
+    // 연결 리스트
     public LinkedList <int> _data3 = new LinkedList<int>();
 
-
     public void init() {
-        
+        _data2.Add(101);
+        _data2.Add(102);
+        _data2.Add(103);
+        _data2.Add(104);
+        _data2.Add(105);
+
+        int tmp = _data2[2];
+        // 'index==2'인 데이터 삭제
+        _data2.RemoveAt(2);
     }
 }
 
---
+++ vsCode/BoardM.cs
@@ -0,0 +1,43 @@
+class BoardM {
+    static void Main(string[] args){
+        Board board = new Board();
+        board.init();
+        
+        // 마지막으로 측정한 시간
+        int lastTick = 0;
+        // command 창에서 커서가 안 보이게 함
+        Console.CursorVisible = false;
+        const int WAIT_TICK = 1/30*1000;
+        // 동그라미 표현식
+        const char CIRCLE = '\u25cf';
+
+        while(true){
+            
+            // Fps 프레임 (60프레임->ok, 30프레임 이하->no)
+            #region 프레임 관리
+            // 현재 시간 정보
+            int crnTick = System.Environment.TickCount;
+
+            // 경과한 시간이 1/30초보다 작다면 -> 단위가 ms라서 1000을 곱해준다.
+            if(crnTick - lastTick < WAIT_TICK)
+                continue;
+            lastTick = crnTick;
+            #endregion
+            
+            // 랜더링
+            // command 창의 커서 위치를 0,0으로 설정
+            Console.SetCursorPosition(0,0);
+            
+            for(int i=0; i<25; i++) {
+                for(int j = 0; j<25; j++) {
+                    // 콘솔 색을 초록색으로 바꿈
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    // 동그라미 (행:25, 열:25) 출력
+                    Console.Write(CIRCLE);
+                }
+                // 개행
+                Console.WriteLine();
+            }
+        }
+    }
+}
